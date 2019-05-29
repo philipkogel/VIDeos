062c0528b7fd0d89baf46a3bbe17d747d6307259
Philip <Philip@philip.com>
2019-05-29 21:16:29 +0200

    Updates

---
+++ precache-manifest.c688b3cd82080814e630866ccb2fa300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea77df6ef45eeceb605fa25634bc5d3",
+    "url": "/VIDeos/index.html"
+  },
+  {
+    "revision": "25db1b4e8e9b3d864252",
+    "url": "/VIDeos/static/css/main.bbaf741b.chunk.css"
+  },
+  {
+    "revision": "a5df55757c061c9c8ac7",
+    "url": "/VIDeos/static/js/2.55aa7153.chunk.js"
+  },
+  {
+    "revision": "25db1b4e8e9b3d864252",
+    "url": "/VIDeos/static/js/main.1473338b.chunk.js"
+  },
+  {
+    "revision": "e33ce21994f84ad5da63",
+    "url": "/VIDeos/static/js/runtime~main.69d0dbfd.js"
+  }
+]);
